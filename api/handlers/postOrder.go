52f882e9fbf3c57ce0b0c6214fb2446517bec6a7
Ren <212835313@qq.com>
2023-07-14 09:58:14 +0800

    完成购买

--- api/handlers/postOrder.go
+++ api/handlers/postOrder.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"Shop/internal/models/view"
+	"Shop/pkg/queries"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +13,10 @@ func PostOrder(c *gin.Context) {
 	}
 	var data ArrayData
 	_ = c.ShouldBindJSON(&data)
-	fmt.Println(data.Values[0].ProductId)
+	fmt.Println(data)
+	if data.Values[0].Count == 0 {
+		data.Values[0].Count = 1
+	}
+	queries.PostOrder(&data.Values)
+	c.JSON(200, "购买成功")
 }
--- pkg/queries/Post.go
+++ pkg/queries/Post.go
@@ -1,1 +1,35 @@
 package queries
+
+import (
+	"Shop/internal/models/view"
+	"Shop/pkg/database"
+	"fmt"
+	"time"
+)
+
+func PostOrder(Order *[]view.ShopCarInfo) {
+	for _, data := range *Order {
+		// 执行数据库插入操作，将data插入到数据库中
+		currentTime := time.Now().Unix()
+		datetime := time.Unix(currentTime, 0).Format("2006-01-02 15:04:05")
+		ret, err := database.Db.Exec("INSERT INTO `order`(create_time, user_id, product_id, status, count, price) VALUES(?,?,?,?,?,?)", datetime, 1, data.ProductId, "已完成", data.Count, data.Count*data.Price)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(ret)
+		res, err := database.Db.Exec("delete from `shop-car` where shop_car_id=?", data.ShopCarInfoID)
+		if err != nil {
+			fmt.Println("exec failed, ", err)
+			return
+		}
+
+		row, err := res.RowsAffected()
+		if err != nil {
+			fmt.Println("rows failed, ", err)
+		}
+
+		fmt.Println("delete succ: ", row)
+	}
+
+}
